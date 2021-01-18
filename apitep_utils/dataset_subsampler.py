054633a1ead7ed1ab33fc74e672e0c6399b3b38c
Juan Diego Gutiérrez Gallardo <andy@unex.es>
2021-01-18 13:12:07 +0100

    Refactor class name

--- apitep_utils/dataset_subsampler.py
+++ apitep_utils/dataset_subsampler.py
@@ -1,3 +1,4 @@
-class Subsampler:
+class DatasetSubsampler:
+
     def __init__(self):
         pass
---
+++ apitep_utils/tests/dataset_subsampler_tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+from apitep_utils import DatasetSubsampler
+
+
+class TestDatasetSubsampler(unittest.TestCase):
+
+    def test_dataset_subsampler(self):
+        dataset_subsampler = DatasetSubsampler()
+        self.assertTrue(False, "This test always fails")
